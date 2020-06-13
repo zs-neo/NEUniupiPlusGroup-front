6b1c049c8e3af36e563e4c2181da73e94cc96c28
chanchenwee <chanchenwee@gmail.com>
2020-06-14 00:13:33 +0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
+	<router-view>
+		
     <router-view/>
   </div>
 </template>
-
 <style lang="less">
 	*{
 		margin: 0;
@@ -12,37 +12,10 @@
 	}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-=======
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-  
-</template>
-
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
->>>>>>> eb9e565cdac24fdf44e32594b7cee0ca6556c5aa
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-<<<<<<< HEAD
   height: calc(100vh);
-=======
-  margin-top: 60px;
->>>>>>> eb9e565cdac24fdf44e32594b7cee0ca6556c5aa
 }
 </style>
