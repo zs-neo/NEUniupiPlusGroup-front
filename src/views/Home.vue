1c28e002e2308f23063cbd26701897be81c7e393
chanchenwee <chanchenwee@gmail.com>
2020-06-13 23:57:46 +0800

    new-2

---
+++ src/views/Home.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="home">
+      <div class="home_head">
+          <div class="headpic">
+              <el-avatar :src="pic"></el-avatar>
+          </div>
+          <div class="headname">
+              {{nickname}}
+          </div>
+
+      </div>
+      <div class="home_content">
+          <div class="home_content_left">
+
+          </div>
+          <div class="home_content_right">
+              <router-view></router-view>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+
+    import HomeService from './HomeService';
+	import SideBar from '@/src/components/homePage/SideBar.vue';
+    export default {
+        data() {
+            return {
+                nickname:'',
+                pic:'',
+            }
+        },
+		components:{
+			SideBar,
+		},
+        computed: {
+
+        },
+        created(){
+			/* 
+			判断用户是否登录以及获取用户信息
+			if(!this.$store.getters.getUserState){
+				 this.$message.error('请先登录！！！');
+				 this.$router.push({name:'Login'});
+			}
+            HomeService.getUser(r=>{
+                console.log(r);
+				this.nickname = r.nickname;
+				this.pic = 'http://localhost:8080/ajax-server/upload/'+r.pic;
+				//http://localhost:8080/ajax-server/userinfo
+            }); */
+        },
+        methods: {
+           
+        }
+    }
+</script>
+
+<style lang="less">
+
+    .home{
+        width:100%;
+        height:100%;
+        display: flex;
+        flex-direction: column;
+        .home_head{
+            flex:1;
+			width: 100%;
+            background-color: lightskyblue;
+            display: flex;
+            >div{
+                height: 40px;
+                -background-color: red;
+                margin: 5px;
+                margin-top: 20px;
+            }
+            .headpic{
+
+            }
+            .headname{
+                line-height: 40px;
+            }
+        }
+        .home_content{
+            flex:8;
+        
+            display: flex;
+			height: 100%;
+            .home_content_left{
+                flex:1;
+				background-color: #324157;
+				text-align: left;
+				
+            }
+            .home_content_right{
+                flex:5;
+          
+            }
+        }
+    }
+
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,88 @@
+<template>
+	<div id="loginFrame">
+		<div id="loginInfo">
+			<div><h1 style="color: cadetblue;">用户登录</h1></div>
+			<div class="errorMsg"><span>{{msg}}</span></div>
+			<div class="inputArea">账号：<input v-model="userinfo.loginname" type="text"></div>
+			<div class="inputArea">密码：<input v-model="userinfo.pwd" type="text"></div>
+			<hr >
+			<div><button type="button" @click="login">登录</button></div>
+		</div>
+	</div>
+</template>
+
+<script>
+	import Vue from 'vue';
+	export default{
+		name:'login',
+		data(){
+			return{
+				userinfo:{
+					loginname:'',
+					pwd:'',
+				},
+				msg:'',
+			};
+		},
+		methods:{
+			//开发中假登录
+			/* login:function(){
+				Vue.axios.get(`http://localhost:8080/ajax-server/userinfo/login`, {params:{
+					'usercode':this.userinfo.loginname,
+					'userpwd':this.userinfo.pwd,
+				}}).then(rs=>{
+					if(rs.data.isLogin){
+						this.$store.commit('validUser');
+						this.$router.push({name:'Home',});
+					}else{
+						this.errorMsg = "密码错误！";
+					}
+				});
+			} */
+			login:function(){
+				this.$router.push({name:'Home',});
+			},
+		},
+	}
+</script>
+
+<style>
+
+	#loginFrame{
+		background-image: url(../assets/img.jpg);
+		background-repeat: no-repeat;
+		background-size: cover;
+		position: absolute;
+		height: 100%;
+		width: 100%;
+	}
+	#loginInfo{
+		background-color: aquamarine;
+		border-radius: 8px;
+		border: 1px solid black;
+		width: 300px;
+		height: 200px;
+		text-align: center;
+		margin: 0 auto;
+		margin-top: 250px;
+		background-color: rgba(240, 255, 255, 0.6);
+
+	}
+	#loginInfo button{
+		text-align: center;
+		border-radius:5px;
+		border: 2px solid #ADD8E6; 
+		background-color: cyan; 
+		width: 20%;
+		height: 35px;
+		margin: 5px;
+	}
+	#loginInfo .errorMsg{
+		color: red;
+		font-weight: 700;
+	}
+	#loginInfo .inputArea{
+		height: 40px;
+		margin: 10px;
+	}
+</style>
