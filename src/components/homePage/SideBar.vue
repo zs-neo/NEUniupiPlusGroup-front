c3c0604d1943d73bb0a498e40a24cee5b6c7fac1
chanchenwee <chanchenwee@gmail.com>
2020-06-14 08:53:34 +0800

    update

--- src/components/homePage/SideBar.vue
+++ src/components/homePage/SideBar.vue
@@ -7,36 +7,48 @@
 		  @close="handleClose"
 		  background-color="#324157"
 		  text-color="#fff"
+		  :unique-opened=true
 		  active-text-color="#20a0ff">
 		  <el-submenu index="1">
 			<template slot="title">
 			  <i class="el-icon-location"></i>
-			  <span>导航一</span>
+			  <span>美食信息管理</span>
 			</template>
-			<el-menu-item-group>
-			  <template slot="title">分组一</template>
-			  <el-menu-item index="1-1">选项1</el-menu-item>
-			  <el-menu-item index="1-2">选项2</el-menu-item>
-			</el-menu-item-group>
-			<el-menu-item-group title="分组2">
-			  <el-menu-item index="1-3">选项3</el-menu-item>
-			</el-menu-item-group>
-			<el-submenu index="1-4">
-			  <template slot="title">选项4</template>
-			  <el-menu-item index="1-4-1">选项1</el-menu-item>
-			</el-submenu>
+			
+			  <el-menu-item index="1-1">
+				  <span>美食管理</span>
+			  </el-menu-item>
+			  <el-menu-item index="1-2">
+				  <span>美食类别管理</span>
+			  </el-menu-item>
 		  </el-submenu>
 		  <el-menu-item index="2">
 			<i class="el-icon-menu"></i>
-			<span slot="title">导航二</span>
+			<span slot="title">活动运维</span>
 		  </el-menu-item>
-		  <el-menu-item index="3" disabled>
+		  <el-menu-item index="3">
 			<i class="el-icon-document"></i>
-			<span slot="title">导航三</span>
+			<span slot="title">系统安全维护</span>
 		  </el-menu-item>
 		  <el-menu-item index="4">
 			<i class="el-icon-setting"></i>
-			<span slot="title">导航四</span>
+			<span slot="title">订单管理</span>
+		  </el-menu-item>
+		  <el-submenu index="5">
+			<template slot="title">
+				<i class="el-icon-setting"></i>
+				<span slot="title">角色和权限管理</span>
+			</template>
+			<el-menu-item index="5-1">
+				<span>角色管理</span>
+			</el-menu-item>
+			<el-menu-item index="5-2">
+				<span>权限管理</span>
+			</el-menu-item>
+		  </el-submenu>
+		  <el-menu-item index="6">
+			<i class="el-icon-setting"></i>
+			<span slot="title">统计报表</span>
 		  </el-menu-item>
 		</el-menu>
 	</div>
