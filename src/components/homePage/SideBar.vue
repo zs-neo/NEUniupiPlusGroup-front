45c2eaf6045474fa7643fb97c85cd5c9a2bee576
chanchenwee <chanchenwee@gmail.com>
2020-06-15 15:18:25 +0800

    update-sideBar-icon

--- src/components/homePage/SideBar.vue
+++ src/components/homePage/SideBar.vue
@@ -1,54 +1,35 @@
 <template>
 	<div class="sideBar">
-		<el-menu class="sidebar-el-menu" background-color="#324157" text-color="#bfcbd9" active-text-color="#20a0ff" :router="true">
-			<el-submenu index="1">
+		
+		<el-menu class="sidebar-el-menu"  background-color="#324157"
+		    text-color="#bfcbd9" active-text-color="#20a0ff"  :router="true">
+		  <el-submenu index="1">
+			<template slot="title">
+			  <i class="el-icon-fork-spoon"></i>
+			  <span>美食信息管理</span>
+			</template>
+			
+			  <el-menu-item index="2">
+				  <span>美食管理</span>
+			  </el-menu-item>
+			  <el-menu-item index="1-2">
+				  <span>美食类别管理</span>
+			  </el-menu-item>
+		   </el-submenu>
+	           <el-submenu index="2">
 				<template slot="title">
-					<i class="el-icon-location"></i>
-					<span>美食信息管理</span>
-				</template>
-				<el-menu-item index="2">
-					<span>美食管理</span>
-				</el-menu-item>
-				<el-menu-item index="1-2">
-					<span>美食类别管理</span>
-				</el-menu-item>
-			</el-submenu>
-			<el-submenu index="2">
-				<template slot="title">
-					<i class="el-icon-location"></i>
-					<span>活动管理</span>
+				
+				  <i class="el-icon-document"></i>
+				  <span>活动管理</span>
 				</template>
 
-				<el-menu-item index="/home/EventsManage">
-					<span>限时活动</span>
-				</el-menu-item>
-				<el-menu-item index="/home/CountMange">
-					<span>优惠券</span>
-				</el-menu-item>
-			</el-submenu>
-			<el-menu-item index="3">
-				<i class="el-icon-document"></i>
-				<span slot="title">系统安全维护</span>
+				<i class="el-icon-user"></i>
+
 			</el-menu-item>
-			<el-menu-item index="4">
-				<i class="el-icon-setting"></i>
-				<span slot="title">订单管理</span>
-			</el-menu-item>
-			<el-submenu index="5">
-				<template slot="title">
-					<i class="el-icon-setting"></i>
-					<span slot="title">角色和权限管理</span>
-				</template>
-				<el-menu-item index="/home/rolesManage">
-					<span>角色管理</span>
-				</el-menu-item>
-				<el-menu-item index="5-2">
-					<span>权限管理</span>
-				</el-menu-item>
-			</el-submenu>
+
 			<el-submenu index="6">
 				<template slot="title">
-					<i class="el-icon-edit"></i>
+					<i class="el-icon-files"></i>
 					<span slot="title">统计报表</span>
 				</template>
 				<el-menu-item index="/home/statisticsManage">
@@ -81,6 +62,7 @@
 
 }
 <style scoped>
+
 	.sidebar {
 		display: block;
 		position: absolute;
